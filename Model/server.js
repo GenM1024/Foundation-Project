eef635496d20d72775a8ac596d87b131ac907e38
[Eghen Mardaymootoo] <[emar0068@student.monash.edu]>
2025-12-09 16:06:00 +0800

    Changes to sqlite3 , to improve runnability

--- Model/server.js
+++ Model/server.js
@@ -1,4 +1,4 @@
-const Database = require("better-sqlite3");
+const Database = require("better-sqlite3-multiple-ciphers");
 const Express = require("express");
 const cors = require("cors");
 const path = require("path");
